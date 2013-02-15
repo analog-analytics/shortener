c0bcf965a709964a11340de5d1253064bbb2d6ad
Daniel Zajic <danielzajic@gmail.com>
2013-02-15 16:41:20 -0700

    Allow passing custom host to #short_url helper

--- app/helpers/shortener/shortener_helper.rb
+++ app/helpers/shortener/shortener_helper.rb
@@ -1,9 +1,16 @@
 module Shortener::ShortenerHelper
 
   # generate a url from a url string
-  def short_url(url, owner=nil)
+  def short_url(url, owner=nil, options={})
     short_url = Shortener::ShortenedUrl.generate(url, owner)
-    short_url ? url_for(:controller => :"shortener/shortened_urls", :action => :show, :id => short_url.unique_key, :only_path => false) : url
+    url_for_options = {
+        :controller => :"shortener/shortened_urls",
+        :action => :show,
+        :id => short_url.unique_key,
+        :only_path => false
+    }
+    url_for_options[:host] = options[:host] if options[:host]
+    short_url ? url_for(url_for_options) : url
   end
 
 end
--- spec/helpers/shortener_helper_spec.rb
+++ spec/helpers/shortener_helper_spec.rb
@@ -8,5 +8,9 @@ describe Shortener::ShortenerHelper do
     it "should shorten the url" do
       helper.short_url("www.doorkeeperhq.com").should == "http://test.host/#{@short_url.unique_key}"
     end
+
+    it "should allow setting the url host" do
+      helper.short_url("www.doorkeeperhq.com", nil, :host => "customhost.com").should == "http://customhost.com/#{@short_url.unique_key}"
+    end
   end
 end
